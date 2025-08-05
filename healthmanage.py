cebdef22b34aa261f704693ef223be6a64384dfb
Pankaj Juyal <80092476+pjuyal007@users.noreply.github.com>
2025-08-05 12:34:12 +0530

    Update healthmanage.py

--- healthmanage.py
+++ healthmanage.py
@@ -8,62 +8,94 @@ lr=int(input())
 c=int(input("enter the client name : 1. for Pankaj 2. for Neeraj 3. for Kunal: \n"))
 # de=int(input("enter diet or workout. 1 for diet 2. for workout"))
 if lr==1:
-    print("what do you want to log workout or diet ")
-    de = int(input("enter 1 for diet 2. for workout"))
+    print("what do you want to log workout plan or diet plan.Type 'finish' in last line of input when your plan is finished ")
+    de = int(input("enter 1 for diet 2. for workout : "))
     if c==1 and de==1:
+        diets = []
         f=open("pdi.txt","a")#a to append /write in file
-        die1=input("diet plan for pankaj:\n")
-        time=str(getdate())#optional we can directly call function getdate
-        f.write(die1+"["+time+"]"+"\n")#f.write(f"{die1} [{time}] \n") better and efficient
+        print("diet plan for pankaj:")
+        while True:
+          die1 = input()
+          if die1.lower()== 'finish':
+              break#exit from while loop
+          diets.append(die1)
+        die2= '\n'.join(diets)#joims the strings with the seperator we want
+
+        f.write(f"{die2} [{getdate()}] \n")
         f.close()
         print("successfully logged diet plan for pankaj")
-        """
-          if c == 1 and de == 1:
-        with open("pdi.txt", "a") as f:
-            entry = input("Diet plan for Pankaj:\n")
-            f.write(f"{entry} [{getdate()}]\n")
-        print("Successfully logged diet plan for Pankaj.")
-        """
     elif c==1 and de==2:
-        f1=open("pwork.txt","a")
-        wo1=input("workout plan for pankaj :\n")
-        time=str(getdate())
-        f1.write(wo1+"["+time+"]"+"\n")
-        f1.close()
+        works = []
+        f=open("pwork.txt","a")
+        print("workout plan for pankaj")
+        while True:
+           wo1 = input()
+           if wo1.lower()=='finish':
+                break
+           works.append(wo1)
+        wo2="\n".join(works)
+        f.write(f" {wo2} {getdate()}:\n")
+        f.close()
         print("successfully logged workout plan for pankaj")
-    elif c==2 and de==1:
-        f2 = open("ndi.txt", "a")
-        die2 = input("diet plan for neeraj:\n")
-        time = str(getdate())
-        f2.write(die2 + "[" + time + "]" + "\n")
-        f2.close()
+    if c==2 and de==1:
+        diets = []
+        f=open("ndi.txt","a")#a to append /write in file
+        print("diet plan for neeraj:")
+        while True:
+          die1 = input()
+          if die1.lower()== 'finish':
+              break
+          diets.append(die1)
+        die2= '\n'.join(diets)#joims the strings with the seperator we want
+
+        f.write(f"{die2} [{getdate()}] \n")
+        f.close()
         print("successfully logged diet plan for neeraj")
     elif c==2 and de==2:
-        f3 = open("nwork.txt", "a")
-        wo2 = input("workout plan for neeraj :\n")
-        time = str(getdate())
-        f3.write(wo2 + "[" + time + "]" + "\n")
-        f3.close()
+        works = []
+        f=open("nwork.txt","a")
+        print("workout plan for neeraj")
+        while True:
+           wo1 = input()
+           if wo1.lower()=='finish':
+                break
+           works.append(wo1)
+        wo2="\n".join(works)
+        f.write(f" {wo2} {getdate()}:\n")
+        f.close()
         print("successfully logged workout plan for neeraj")
-    elif c==3 and de==1:
-        f4 = open("kdi.txt", "a")
-        die3 = input("diet plan for kunal:\n")
-        time = str(getdate())
-        f4.write(die3 + "[" + time + "]" + "\n")
-        f4.close()
+    if c==3 and de==1:
+        diets = []
+        f=open("kdi.txt","a")#a to append /write in file
+        print("diet plan for kunal:")
+        while True:
+          die1 = input()
+          if die1.lower()== 'finish':
+              break
+          diets.append(die1)
+        die2= '\n'.join(diets)#joims the strings with the seperator we want
+
+        f.write(f"{die2} [{getdate()}] \n")
+        f.close()
         print("successfully logged diet plan for kunal")
     elif c==3 and de==2:
-        f5 = open("kwork.txt", "a")
-        wo3 = input("workout plan for kunal :\n")
-        time = str(getdate())
-        f5.write(wo3 + "[" + time + "]" + "\n")
-        f5.close()
+        works = []
+        f=open("kwork.txt","a")
+        print("workout plan for kunal")
+        while True:
+           wo1 = input()
+           if wo1.lower()=='finish':
+                break
+           works.append(wo1)
+        wo2="\n".join(works)
+        f.write(f" {wo2} {getdate()}:\n")
+        f.close()
         print("successfully logged workout plan for kunal")
     else:
         print("Enter valid details ")
 import os
 if lr==2 :
-    print("what do you want to retrieve diet or wokout:")
+    print("what do you want to retrieve diet or workout :")
     de = int(input("enter 1 for diet 2. for workout"))
     if c==1 and de ==1:
         if os.path.exists("pdi.txt"):
@@ -101,6 +133,7 @@ if lr==2 :
                 print(f.read())
         else:
             print("No logs found yet for kunal's workout.")
+
 
 
 
